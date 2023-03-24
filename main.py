cffd2adba96f04ba2fdf2690117aa300327d2c42
HickerDicker <113861320+HickerDicker@users.noreply.github.com>
2023-03-24 21:46:25 +0000

    Add files via upload
    new update with a fix for nvme ssds

--- main.py
+++ main.py
@@ -9,14 +9,26 @@ disk = input('What partiton would you like to install HickOS on? make sure you m
 
 os.system('cgdisk /dev/' + disk)
 
-os.system('mkfs.ext4 /dev/'+disk + "3")
-os.system('mkswap /dev/'+disk + "2")
-os.system('mkfs.vfat -F32 /dev/'+disk + "1")
-os.system('swapon /dev/'+disk + "2")
-os.system('mount /dev/'+disk + '3 /mnt')
-os.system('mkdir -p /mnt/boot')
-os.system('mount /dev/'+disk + "1 /mnt/boot")
-os.system('mkdir /mnt/stuff')
+substring="nvme"
+
+if substring in disk:
+    os.system('mkfs.ext4 /dev/' + disk + "p3")
+    os.system('mkswap /dev/' + disk + "p2")
+    os.system('mkfs.vfat -F32 /dev/' + disk + "p1")
+    os.system('swapon /dev/' + disk + "p2")
+    os.system('mount /dev/' + disk + 'p3 /mnt')
+    os.system('mkdir -p /mnt/boot')
+    os.system('mount /dev/' + disk + "p1 /mnt/boot")
+    os.system('mkdir /mnt/stuff')
+else:
+    os.system('mkfs.ext4 /dev/'+disk + "3")
+    os.system('mkswap /dev/'+disk + "2")
+    os.system('mkfs.vfat -F32 /dev/'+disk + "1")
+    os.system('swapon /dev/'+disk + "2")
+    os.system('mount /dev/'+disk + '3 /mnt')
+    os.system('mkdir -p /mnt/boot')
+    os.system('mount /dev/'+disk + "1 /mnt/boot")
+    os.system('mkdir /mnt/stuff')
 
 os.system('pacstrap -K /mnt base base-devel linux-zen linux-zen-headers linux-firmware vim dhcpcd efibootmgr grub lightdm lightdm-gtk-greeter nano vim vi flatpak git neofetch networkmanager pipewire pipewire-pulse pipewire-jack pipewire-alsa alsa-utils wireplumber firefox')
 
@@ -27,4 +39,3 @@ os.system('curl -o /mnt/stuff/chroot.py https://hickos.hickdick.workers.dev/0:/c
 os.system('chmod 755 /mnt/stuff/chroot.py')
 
 os.system('arch-chroot /mnt /stuff/chroot.py')
-
